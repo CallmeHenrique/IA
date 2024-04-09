7cfd635e4aa1d2d3f2a3355be5db63f58d67aa14
Henrique Costa <86430925+CallmeHenrique@users.noreply.github.com>
2024-04-08 21:16:28 -0300

    Create Teste5_Arquitetura1.py

---
+++ Teste5_Arquitetura1.py
@@ -0,0 +1,34 @@
+arquivo = np.load('teste5.npy')
+x = arquivo[0]
+y = np.ravel(arquivo[1])
+
+regr = MLPRegressor(hidden_layer_sizes=(5, 10),
+                    max_iter=100000,
+                    activation='relu', #{'identity', 'logistic', 'tanh', 'relu'},
+                    solver='adam',
+                    learning_rate = 'adaptive',
+                    n_iter_no_change=500)
+
+regr = regr.fit(x,y)
+y_est = regr.predict(x)
+erro = np.abs(y_est - y) #Erro absoluto
+
+print("Desvio Padrão: ", np.std(erro)) #Desvio Padrão do Erro
+print("Média: ", np.mean(erro)) #Média do Erro
+
+plt.figure(figsize=[14,7])
+
+#plot curso original
+plt.subplot(1,3,1)
+plt.plot(x,y)
+
+#plot aprendizagem
+plt.subplot(1,3,2)
+plt.plot(regr.loss_curve_)
+
+#plot regressor
+plt.subplot(1,3,3)
+plt.plot(x,y,linewidth=1,color='yellow')
+plt.plot(x,y_est,linewidth=2)
+
+plt.show()
